5f31c09eefef59b56b30691a2994bc99da963267
ismael <joaoism45@gmail.com>
2021-01-27 22:43:47 -0300

    aplicando boas platicas no codigo

--- main.tf
+++ main.tf
@@ -1,25 +1,10 @@
-terraform {
-  required_providers {
-    google = {
-      source = "hashicorp/google"
-    }
-  }
-}
-
-# Configura o Provider Google Cloud com o Projeto
-provider "google" {
-  version = "3.5.0"
-
-  project =  "ismael-cloudautomation"
-  region  = "us-central1"
-  zone    = "us-central1-c"
-}
+#### RECURSOS NO GCP #####
 
 # Cria uma VM no Google Cloud
 resource "google_compute_instance" "firstvm" {
   name         = "website"
   machine_type = "n1-standard-1"
-  zone         = "us-central1-c"
+  zone         = var.gcp_zone
   tags         = ["website", "impacta"]
 
   # Defini a Imagem da VM
@@ -34,12 +19,7 @@ resource "google_compute_instance" "firstvm" {
     network = "default" # Estamos usando a VPC default que já vem por padrão no projeto.
 
     access_config {
-      // A presença do bloco access_config, mesmo sem argumentos, garante que a instância estará acessível pela internet.
     }
+    // A presença do bloco access_config, mesmo sem argumentos, garante que a instância estará acessível pela internet.
   }
 }
-# Retorna o IP da VM criada
-output "ip" {
-  value = google_compute_instance.firstvm.network_interface.0.access_config.0.nat_ip
-}
-
---
+++ outputs.tf
@@ -0,0 +1,5 @@
+# Retorna o IP da VM criada
+output "ip" {
+  value = google_compute_instance.firstvm.network_interface.0.access_config.0.nat_ip
+}
+
